3393c8d96af839531d41639cb524735e0889426b
Dagim <dagimed6@gmail.com>
2025-05-25 12:55:33 +0400

    Add basic_operations.py for arithmetic operations

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,15 +1,13 @@
-# basic_operations.py
-
 # Assign values to variables
-num_a = 10
-num_b = 5
+number1 = 10
+number2 = 5
 
 # Perform operations
-add = num_a + num_b
-sub = num_a - num_b
-mult = num_a * num_b
+add = number1 + number2
+sub = number1 - number2
+mult = number1 * number2
 
 # Print results
-print(f"Addition of {num_a} and {num_b} is {add}")
-print(f"Subtraction of {num_a} and {num_b} is {sub}")
-print(f"Multiplication of {num_a} and {num_b} is {mult}")
+print(f"Addition of {number1} and {number2} is {add}")
+print(f"Subtraction of {number1} and {number2} is {sub}")
+print(f"Multiplication of {number1} and {number2} is {mult}")
